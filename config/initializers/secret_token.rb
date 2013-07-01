b3a3db53b883ab68c2b62c93fdcdd10750cd8874
Wending Lu <wending@peek.com>
2013-07-01 16:04:48 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Carbon::Application.config.secret_key_base = '25ea32cc04b3fa55de0f71b40917b28b89c9a7628713167726f202e64b7e0078b53c9e8f1bed04efa380688ea5dc2537a93e7190df846564a323177d1bd11737'
